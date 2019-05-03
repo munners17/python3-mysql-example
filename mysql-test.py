d4ac2aad6e8c1597234d44632bf1a287dfe4f73e
Luis Aguilar <luis@berkeley.edu>
2019-05-03 11:21:05 -0700

    Adding a list and detail page along with some dummy buttons.

--- mysql-test.py
+++ mysql-test.py
@@ -7,25 +7,30 @@ app = Flask(__name__)
 class Database:
     def __init__(self):
         host = "127.0.0.1"
-        user = "test"
-        password = "password"
+        user = "root"
+        #password = "password"
         db = "employees"
 
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+        self.con = pymysql.connect(host=host, user=user, db=db, cursorclass=pymysql.cursors.
                                    DictCursor)
         self.cur = self.con.cursor()
 
     def list_employees(self):
-        self.cur.execute("SELECT first_name, last_name, gender FROM employees LIMIT 50")
+        self.cur.execute("SELECT id, first_name, last_name FROM employees LIMIT 50")
         result = self.cur.fetchall()
-
         return result
+
+    def employee_detail(self, id):
+        self.cur.execute("SELECT * FROM employees WHERE id = %s", id)
+        result = self.cur.fetchall()
+        return result
+db = Database()
 
 @app.route('/')
 def employees():
 
     def db_query():
-        db = Database()
+        #db = Database()
         emps = db.list_employees()
 
         return emps
@@ -34,3 +39,7 @@ def employees():
 
     return render_template('employees.html', result=res, content_type='application/json')
 
+@app.route('/employee_detail/<id>')
+def employee_detail(id):
+    res = db.employee_detail(id)
+    return render_template('employee_detail.html', result=res, content_type='application/json')    
